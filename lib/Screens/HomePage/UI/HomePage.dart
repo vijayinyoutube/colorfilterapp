8380c76dbbf5e57a883ef12d2a7bda7aebf84122
Vijay <vijaythevirtue@gmail.com>
2021-03-29 23:18:54 +0530

    UI Changes - DONE

--- lib/Screens/HomePage/UI/HomePage.dart
+++ lib/Screens/HomePage/UI/HomePage.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -11,7 +13,7 @@ List mycolors = <Color>[
   Colors.green,
   Colors.purple,
   Colors.orange,
-  Colors.indigo
+  Colors.indigo,
 ];
 Color primaryColor = mycolors[0];
 
@@ -22,12 +24,12 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text("Color Filters"),
       ),
-      backgroundColor: Colors.white,
       body: Center(
         child: Stack(
           children: [
             buildImage(),
             buildColorIcons(),
+            buildAmountTag(),
           ],
         ),
       ),
@@ -41,46 +43,67 @@ class _MyHomePageState extends State<MyHomePage> {
           colorFilter: ColorFilter.mode(primaryColor, BlendMode.hue),
           child: Container(
             color: Colors.white,
-            child: Image.asset(
-              "assets/Images/product.png",
-              fit: BoxFit.contain,
+            child: Transform.translate(
+              offset: Offset(35, -30),
+              child: Transform.rotate(
+                angle: pi / -0.55,
+                child: Image.asset(
+                  "assets/Images/product.png",
+                  fit: BoxFit.contain,
+                ),
+              ),
             ),
           ),
         ),
       );
 
   Widget buildColorIcons() => Positioned(
-        bottom: 100,
-        right: 50,
+        bottom: 35,
+        right: 10,
         child: Row(
           children: [for (var i = 0; i < 6; i++) buildIconBtn(mycolors[i])],
         ),
       );
 
   Widget buildIconBtn(Color myColor) => Container(
-          child: Stack(
-        children: [
-          Positioned(
-            top: 12.5,
-            left: 12.5,
-            child: Icon(
-              Icons.check,
-              size: 20,
-              color: primaryColor == myColor ? myColor : Colors.transparent,
+        child: Stack(
+          children: [
+            Positioned(
+              top: 12.5,
+              left: 12.5,
+              child: Icon(
+                Icons.check,
+                size: 20,
+                color: primaryColor == myColor ? myColor : Colors.transparent,
+              ),
             ),
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.circle,
-              color: myColor.withOpacity(0.65),
-              size: 30,
+            IconButton(
+              icon: Icon(
+                Icons.circle,
+                color: myColor.withOpacity(0.65),
+                size: 30,
+              ),
+              onPressed: () {
+                setState(() {
+                  primaryColor = myColor;
+                });
+              },
             ),
-            onPressed: () {
-              setState(() {
-                primaryColor = myColor;
-              });
-            },
+          ],
+        ),
+      );
+
+  Widget buildAmountTag() => Positioned(
+        bottom: 125,
+        left: 50,
+        child: Container(
+          child: Text(
+            "\$25.00",
+            style: TextStyle(
+                color: primaryColor.withOpacity(0.65),
+                fontSize: 30.00,
+                fontWeight: FontWeight.w500),
           ),
-        ],
-      ));
+        ),
+      );
 }
