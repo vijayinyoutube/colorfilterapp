69a62fa3df8acea29595e2027511f4e4bc245494
Vijay <vijaythevirtue@gmail.com>
2021-03-29 22:22:20 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/colorfilterapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.colorfilterapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Screens/HomePage/UI/HomePage.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+List mycolors = <Color>[
+  Colors.red,
+  Colors.blue,
+  Colors.green,
+  Colors.purple,
+  Colors.orange,
+  Colors.indigo
+];
+Color primaryColor = mycolors[0];
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Color Filters"),
+      ),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Stack(
+          children: [
+            buildImage(),
+            buildColorIcons(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildImage() => Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: ColorFiltered(
+          colorFilter: ColorFilter.mode(primaryColor, BlendMode.hue),
+          child: Container(
+            color: Colors.white,
+            child: Image.asset(
+              "assets/Images/product.png",
+              fit: BoxFit.contain,
+            ),
+          ),
+        ),
+      );
+
+  Widget buildColorIcons() => Positioned(
+        bottom: 100,
+        right: 50,
+        child: Row(
+          children: [for (var i = 0; i < 6; i++) buildIconBtn(mycolors[i])],
+        ),
+      );
+
+  Widget buildIconBtn(Color myColor) => Container(
+          child: Stack(
+        children: [
+          Positioned(
+            top: 12.5,
+            left: 12.5,
+            child: Icon(
+              Icons.check,
+              size: 20,
+              color: primaryColor == myColor ? myColor : Colors.transparent,
+            ),
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.circle,
+              color: myColor.withOpacity(0.65),
+              size: 30,
+            ),
+            onPressed: () {
+              setState(() {
+                primaryColor = myColor;
+              });
+            },
+          ),
+        ],
+      ));
+}
